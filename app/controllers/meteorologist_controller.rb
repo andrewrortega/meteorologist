a6bf7be4640b913aee12b14be8149e09ecd696da
Andrew Ortega <andrewrortega@chicagobooth.edu>
2017-04-19 18:11:23 -0500

    final update to fix coordinate generation from address to wx

--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -15,17 +15,33 @@ class MeteorologistController < ApplicationController
     # The street address that the user typed is in the variable @street_address.
     # ==========================================================================
 
+    @street_address_parsed = @street_address.gsub(" ","+")
 
+    @url_maps = "http://maps.googleapis.com/maps/api/geocode/json?address="
 
-    @current_temperature = "Replace this string with your answer."
+    parsed_data_maps = JSON.parse(open(@url_maps+@street_address_parsed).read)
 
-    @current_summary = "Replace this string with your answer."
+    @latitude = parsed_data_maps["results"][0]["geometry"]["location"]["lat"]
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @longitude = parsed_data_maps["results"][0]["geometry"]["location"]["lng"]
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    url_cast = "https://api.darksky.net/forecast/7fb23f704e62590627e6994f6cc92050/"
+    #
+    latitude_comma=@latitude.to_s+","
+    #
+    longitude_end=@longitude.to_s
+    #
+    parsed_data_cast_with_map_address = JSON.parse(open(url_cast+latitude_comma+longitude_end).read)
 
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @current_temperature = parsed_data_cast_with_map_address["currently"]["temperature"]
+    #
+    @current_summary = parsed_data_cast_with_map_address["currently"]["summary"]
+    #
+    @summary_of_next_sixty_minutes = parsed_data_cast_with_map_address["minutely"]["summary"]
+    #
+    @summary_of_next_several_hours = parsed_data_cast_with_map_address["hourly"]["summary"]
+    #
+    @summary_of_next_several_days = parsed_data_cast_with_map_address["daily"]["summary"]
 
     render("meteorologist/street_to_weather.html.erb")
   end
